ab3abc0834c78964fa14009e951fefa84a846c4f
Francois Raminosona <francois.raminosona@laerdal.com>
2023-10-03 13:12:45 +0200

    Removing Logging LogLevel

--- Laerdal.Dfu/Enums/DfuLogLevel.cs
+++ Laerdal.Dfu/Enums/DfuLogLevel.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.Logging;
-
 using System;
 
 namespace Laerdal.Dfu.Enums
@@ -17,28 +15,5 @@ namespace Laerdal.Dfu.Enums
         Warning = 15, // 0x000000000000000F
 
         Error = 20 // 0x0000000000000014
-    }
-    
-    public static class DfuLogLevelExtensions 
-    {
-        public static LogLevel ToLogLevel(this DfuLogLevel logLevel)
-        {
-            switch (logLevel)
-            {
-                case DfuLogLevel.Debug:
-                    return LogLevel.Debug;
-                case DfuLogLevel.Verbose:
-                    return LogLevel.Trace;
-                case DfuLogLevel.Info:
-                case DfuLogLevel.Application:
-                    return LogLevel.Information;
-                case DfuLogLevel.Warning:
-                    return LogLevel.Warning;
-                case DfuLogLevel.Error:
-                    return LogLevel.Error;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(logLevel), logLevel, null);
-            }
-        }
     }
 }
