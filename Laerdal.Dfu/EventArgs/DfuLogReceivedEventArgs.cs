57df94d4679d5163efdbaf7096b68889eb8f9b9e
Francois Raminosona <francois.raminosona@laerdal.com>
2023-10-03 13:16:29 +0200

    Removing Logging LogLevel

--- Laerdal.Dfu/EventArgs/DfuLogReceivedEventArgs.cs
+++ Laerdal.Dfu/EventArgs/DfuLogReceivedEventArgs.cs
@@ -1,20 +1,14 @@
 using Laerdal.Dfu.Enums;
 
-using Microsoft.Extensions.Logging;
-
 namespace Laerdal.Dfu.EventArgs
 {
     public class DfuLogReceivedEventArgs : System.EventArgs
     {
-        public LogLevel LogLevel { get; }
+        public DfuLogLevel LogLevel { get; }
 
         public string Message { get; }
 
-        internal DfuLogReceivedEventArgs(DfuLogLevel logLevel, string message) : this(logLevel.ToLogLevel(), message)
-        {
-        }
-        
-        internal DfuLogReceivedEventArgs(LogLevel logLevel, string message)
+        internal DfuLogReceivedEventArgs(DfuLogLevel logLevel, string message)
         {
             LogLevel = logLevel;
             Message = message;
