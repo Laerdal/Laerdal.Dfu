0f8532d861fc3c0d0a26a0f477bfb7843b0f8f75
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2024-05-13 20:10:35 +0200

    feat (Laerdal.Dfu.csproj): add placeholder support for netstandard (throws not-implemented exceptions of course) to make the lives of MAUI projects using this nuget easier
    [skip ci]

---
+++ Laerdal.Dfu/Platforms.NetStandard/DfuInstallation.cs
@@ -0,0 +1,10 @@
+namespace Laerdal.Dfu
+{
+    public partial class DfuInstallation
+    {
+        public override void Abort() => throw new NotImplementedException();
+        public override void Start() => throw new NotImplementedException();
+        public override void Pause() => throw new NotImplementedException();
+        public override void Resume() => throw new NotImplementedException();
+    }
+}
