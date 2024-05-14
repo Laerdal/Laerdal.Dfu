c336cfac4d47d806adee85baf784ca2cc2e1657b
Kyriakos Sidiropoulos <kyriakos.sidiropoulos@laerdal.com>
2024-05-14 18:30:13 +0200

    fix ([NetStandard] DfuInstallation.cs): add a default constructor to truly align the placeholder implementation with the ones of other platforms

--- Laerdal.Dfu/Platforms.NetStandard/DfuInstallation.cs
+++ Laerdal.Dfu/Platforms.NetStandard/DfuInstallation.cs
@@ -2,6 +2,11 @@ namespace Laerdal.Dfu
 {
     public partial class DfuInstallation
     {
+        public DfuInstallation(string deviceId, string fileUrl)
+        {
+            throw new NotImplementedException();
+        }
+        
         public override void Abort() => throw new NotImplementedException();
         public override void Start() => throw new NotImplementedException();
         public override void Pause() => throw new NotImplementedException();
